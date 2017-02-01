c0ad76583f609a8b72057bd04936435f058f06d7
barasher <arnaud.barasher@gmail.com>
2017-02-01 23:25:31 +0100

    Formatting

--- extremestartupng-common/src/main/java/com/barasher/esng/data/Question.java
+++ extremestartupng-common/src/main/java/com/barasher/esng/data/Question.java
@@ -7,49 +7,49 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Question {
 
-    @JsonProperty("_unid")
-    private String _unid;
-    @JsonProperty("_question")
-    private String _question;
+	@JsonProperty("_unid")
+	private String _unid;
+	@JsonProperty("_question")
+	private String _question;
 
-    public Question() {
-	//
-    }
-
-    public Question(String anUnid, String aQuestion) {
-	_unid = anUnid;
-	_question = aQuestion;
-    }
-
-    @Override
-    public boolean equals(Object anotherObject) {
-	if (anotherObject == null || !anotherObject.getClass().equals(Question.class)) {
-	    return false;
+	public Question() {
+		//
 	}
-	final Question other = (Question) anotherObject;
-	boolean equals = Objects.equals(_unid, other._unid);
-	equals = equals && Objects.equals(_question, other._question);
-	return equals;
-    }
 
-    @Override
-    public int hashCode() {
-	return Objects.hash(_unid, _question);
-    }
+	public Question(String anUnid, String aQuestion) {
+		_unid = anUnid;
+		_question = aQuestion;
+	}
 
-    @Override
-    public String toString() {
-	return new StringBuilder("[").append(_unid).append("] ").append(_question).append(" ").toString();
-    }
+	@Override
+	public boolean equals(Object anotherObject) {
+		if (anotherObject == null || !anotherObject.getClass().equals(Question.class)) {
+			return false;
+		}
+		final Question other = (Question) anotherObject;
+		boolean equals = Objects.equals(_unid, other._unid);
+		equals = equals && Objects.equals(_question, other._question);
+		return equals;
+	}
 
-    @JsonIgnore
-    public String getUnid() {
-	return _unid;
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(_unid, _question);
+	}
 
-    @JsonIgnore
-    public String getQuestion() {
-	return _question;
-    }
+	@Override
+	public String toString() {
+		return new StringBuilder("[").append(_unid).append("] ").append(_question).append(" ").toString();
+	}
+
+	@JsonIgnore
+	public String getUnid() {
+		return _unid;
+	}
+
+	@JsonIgnore
+	public String getQuestion() {
+		return _question;
+	}
 
 }
--- extremestartupng-referee/src/main/java/com/barasher/esng/question/impl/SimpleAdditionQuestionProvider.java
+++ extremestartupng-referee/src/main/java/com/barasher/esng/question/impl/SimpleAdditionQuestionProvider.java
@@ -10,24 +10,24 @@ import com.barasher.esng.question.QuestionProvider;
 @QuestionProvider(family = "simpleAddtition", level = 1)
 public class SimpleAdditionQuestionProvider implements IQuestionProvider {
 
-    static String PLUS = " plus ";
+	static String PLUS = " plus ";
 
-    @Override
-    public QuestionContext provide() {
-	final String unid = UUID.randomUUID().toString().substring(0, 8);
-	final int op1 = (int) (Math.random() * 100);
-	final int op2 = (int) (Math.random() * 100);
-	final Question q = new Question(unid, "What is " + op1 + PLUS + op2 + " ?");
-	return new QuestionContext(q, (r) -> {
-	    try {
-		if (r != null) {
-		    return op1 + op2 == Double.parseDouble(r);
-		}
-	    } catch (final NumberFormatException e) {
-		// nothing
-	    }
-	    return false;
-	}, 10, -20);
-    }
+	@Override
+	public QuestionContext provide() {
+		final String unid = UUID.randomUUID().toString().substring(0, 8);
+		final int op1 = (int) (Math.random() * 100);
+		final int op2 = (int) (Math.random() * 100);
+		final Question q = new Question(unid, "What is " + op1 + PLUS + op2 + " ?");
+		return new QuestionContext(q, (r) -> {
+			try {
+				if (r != null) {
+					return op1 + op2 == Double.parseDouble(r);
+				}
+			} catch (final NumberFormatException e) {
+				// nothing
+			}
+			return false;
+		}, 10, -20);
+	}
 
 }
